18f932f5669d4366f3d10c9e418a209316c2f969
Islam77707 <34512851+Islam77707@users.noreply.github.com>
2019-10-14 13:03:53 +0300

    testo.02
    мы справились0.2

---
+++ ConsoleApplication8/ConsoleApplication8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, -12, 124, 1, 4, 5, -1, -6 };
+            int NumPlus;
+            int ReversNum;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    NumPlus = array[i];
+                    Console.WriteLine(NumPlus);
+                } // ДАНИИЛ КРУТОЙ ,А КОРЗИНА КРУГЛАЯ
+                //ReversNum = -array[i];
+                //Console.WriteLine(-array[i]); 
+            }
+            foreach (int i in array)
+            {
+                Console.WriteLine(-(array[i]));
+            }
+            Console.ReadKey();
+        }
+    }
+}
